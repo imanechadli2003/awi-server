c2687ac9a7b6990bcc62b77a5ee6b8ab1f3c736f
imanechadli2003 <imanechadli20003@gmail.com>
2025-01-04 15:05:48 +0100

    cont

--- server/src/routes/depotRoutes.ts
+++ server/src/routes/depotRoutes.ts
@@ -4,23 +4,13 @@ const router = express.Router();
 
 
 router.get('/jeux', getJeux);
-
-// Route pour mettre un jeu en vente
-router.put('/jeux/:id/mettre-en-vente', mettreEnVente);
-
-// Route pour créer un dépôt (vérification de session active et ajout de jeux)
 router.post('/depot', creerDepot);
 
-// Route pour récupérer la liste des vendeurs
+
+router.put('/jeux/:id/mettre-en-vente', mettreEnVente);
 router.get('/vendeurs', getvendeurs);
-
-// Route pour récupérer un vendeur spécifique avec ses dépôts associés
 router.get('/vendeurs/:id', getVendeurById);
-
-// Route pour récupérer toutes les marques de jeux
 router.get('/marques', getAllJeuxMarques);
-
-// Route pour récupérer une marque de jeu spécifique
 router.get('/marques/:id', getJeuxMarqueById);
 
 export default router;
